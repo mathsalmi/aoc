103c552009190e78cae62e0d5a8969ea8e77d6f8
Matheus Salmi <mathsalmi@gmail.com>
2015-12-09 20:09:19 -0200

    Implementing part 2

--- day1/main.go
+++ day1/main.go
@@ -13,16 +13,22 @@ func main() {
 	}
 
 	pos := 0
+	icharbasement := 0
 
 	input := string(bytes)
-	for _, char := range input {
+	for index, char := range input {
 		switch char {
 		case '(':
 			pos++
 		case ')':
 			pos--
 		}
+
+		if pos == -1 && icharbasement == 0 {
+			icharbasement = index
+		}
 	}
 
 	fmt.Println("Santa is on: ", pos)
+	fmt.Println("Basement on char #: ", (icharbasement + 1))
 }
