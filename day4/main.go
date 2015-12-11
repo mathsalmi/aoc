c4be569508a89a006163765f928bda720cbb08e6
Matheus Salmi <mathsalmi@gmail.com>
2015-12-11 00:40:40 -0200

    day4 part1

---
+++ day4/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"crypto/md5"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	i, input := 0, "yzbqklnj"
+
+	hash := ""
+	for ; !strings.HasPrefix(hash, "00000"); i++ {
+		x := md5.Sum([]byte(input + strconv.Itoa(i)))
+		hash = fmt.Sprintf("%x", x[:])
+	}
+
+	fmt.Println("# is ", (i - 1), hash)
+}
