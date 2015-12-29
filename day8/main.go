ee8165fe2a355edbd21e1a3cf9e8bce36ab54d1a
Matheus Salmi <mathsalmi@gmail.com>
2015-12-29 14:58:27 -0200

    day8 part2

--- day8/main.go
+++ day8/main.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"bufio"
-	"bytes"
-	"encoding/hex"
 	"fmt"
 	"os"
-	"strings"
 	"unicode/utf8"
 )
 
@@ -27,43 +24,13 @@ func main() {
 		panic(scanner.Err())
 	}
 
-	fmt.Println("Total ", strlen-vallen)
+	fmt.Println("Total ", vallen-strlen)
 }
 
 func parse(s string) (strlen, vallen int, ss string) {
 	strlen = utf8.RuneCountInString(s)
-
-	bf := bytes.Buffer{}
-	reader := bufio.NewReader(strings.NewReader(s))
-	for {
-		c, err := reader.ReadByte()
-		if err != nil {
-			break
-		}
-
-		if c != '\\' {
-			bf.WriteByte(c)
-		} else {
-			peek, _ := reader.Peek(3)
-			if len(peek) > 0 {
-				nc := peek[0]
-				switch nc {
-				case '\\', '"':
-					bf.WriteByte(nc)
-					reader.Discard(1)
-				case 'x':
-					dst := make([]byte, 1)
-					hex.Decode(dst, peek[1:3])
-
-					bf.Write(dst)
-					reader.Discard(3)
-				}
-			}
-		}
-	}
-
-	vallen = utf8.RuneCountInString(bf.String()) - 2 // remove the literals
-	ss = bf.String()
+	ss = fmt.Sprintf("%q", s)
+	vallen = utf8.RuneCountInString(ss)
 
 	return
 }
